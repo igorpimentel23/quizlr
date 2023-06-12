56ba7d038b1898631d4c90484e4db2bc51b0103b
Igor Pimentel <igorpimentel@id.uff.br>
2023-06-11 22:52:19 -0300

    chore: project initial configs

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@theme': './src/theme',
+            '@types': './src/types',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.svg'],
+        },
+      ],
+    ],
   };
 };
